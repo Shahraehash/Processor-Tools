bb17f91fc1af71c5d5d636f1b493e6512653dfde
bdfennell <brandon.fennell@gmail.com>
2021-09-25 08:33:23 -0700

    imporved validation styles

--- src/components/DataValidation.vue
+++ src/components/DataValidation.vue
@@ -1,64 +1,75 @@
 <template>
   <v-card flat>
-    <div class="text-center" v-if="fileData != null">
-      <div>
-        <v-icon large>mdi-table-column</v-icon>
-        {{fileData.columns}} columns
-        <v-icon large>mdi-table-row</v-icon>
-        {{fileData.rows}} rows
-      </div>
-      <div>
-        <div v-if="errors.missingData">
-          <v-alert dense outlined color="warning" >
-            <v-icon color="orange" >mdi-alert-circle</v-icon>
-            {{fileData.nan_count}} rows are missing data.
-          </v-alert>
+    <div class="text-left" v-if="fileData != null">
+      <v-card class="py-2 px-3 mt-3 mr-5" flat outlined>
+        <div class="overline">File Validation</div>
 
-        </div>
-        <div v-if="!errors.missingData">
-          <v-icon color="green" >mdi-check-circle</v-icon> All rows have complete data.
-        </div>
-      </div>
-
-      <div>
-        <div v-if="errors.textData">
-          <v-alert dense outlined color="error" >
-            <v-icon color="red" >mdi-alert-circle</v-icon>
-            {{fileData.invalid_columns.length}} column has non-numerical data.
-            <div>
-              <v-chip dark small color="red lighten-3" v-for="(item, key) in fileData.invalid_columns" :key="key">{{item}}</v-chip>
+        <!-- Row Errors -->
+        <div>
+          <div v-if="dataType == 'training'">
+            <div v-if="errors.maxTrainingRows">
+              <v-icon color="red" >mdi-alert-circle</v-icon>
+              {{fileData.rows}} rows
+              <span class="grey--text">(Max is {{rules.maxTrainingRows}})</span>
             </div>
+            <div v-if="!errors.maxTrainingRows">
+              <v-icon color="green" >mdi-check-circle</v-icon>
+              {{fileData.rows}} rows
+              <span class="grey--text">(Max is {{rules.maxTrainingRows}})</span>
+            </div>
+          </div>
+          <div v-if="dataType == 'testing'">
+            <div v-if="errors.maxTestingRows">
+              <v-icon color="red" >mdi-alert-circle</v-icon>
+              {{fileData.rows}} rows
+              <span class="grey--text">(Max is {{rules.maxTestingRows}})</span>
+            </div>
+            <div v-if="!errors.maxTestingRows">
+              <v-icon color="green" >mdi-check-circle</v-icon>
+              {{fileData.rows}} rows
+              <span class="grey--text">(Max is {{rules.maxTestingRows}})</span>
+            </div>
+          </div>
+        </div>
+        <!-- Column Errors -->
+        <div>
+          <div v-if="errors.maxFeatures">
+            <v-icon color="red" >mdi-alert-circle</v-icon>
+            {{fileData.columns}} columns
+            <span class="grey--text">(Max is {{rules.maxFeatures}})</span>
+          </div>
+          <div v-if="!errors.maxFeatures">
+            <v-icon color="green" >mdi-check-circle</v-icon>
+            {{fileData.columns}} columns
+            <span class="grey--text">(Max is {{rules.maxFeatures}})</span>
+          </div>
+        </div>
+        <!-- Missing Data Error -->
+        <div>
+          <div v-if="errors.missingData">
+            <v-icon color="orange" >mdi-alert-circle</v-icon>
+            {{fileData.nan_count}} rows are missing data. These rows cannot be used.
+          </div>
+          <div v-if="!errors.missingData">
+            <v-icon color="green" >mdi-check-circle</v-icon> All rows have complete data.
+          </div>
+        </div>
+        <!-- Text Data Error -->
+        <div>
+          <div v-if="errors.textData">
+            <v-icon color="orange" >mdi-alert-circle</v-icon>
+            {{fileData.invalid_columns.length}} column<span v-if="fileData.invalid_columns.length > 1">s</span> (
+            <v-chip dark small color="orange lighten-3" v-for="(item, key) in fileData.invalid_columns" :key="key">{{item}}</v-chip>
+            ) has non-numerical data. MILO can only use numerical data.
 
-          </v-alert>
-        </div>
-        <div v-if="!errors.textData">
-          <v-icon color="green" >mdi-check-circle</v-icon> All rows have numerical data.
-        </div>
-      </div>
 
-      <div>
-        <div v-if="errors.maxFeatures">
-          <v-alert dense outlined color="error">
-            Data set has {{fileData.columns}} columns. Max permitted is {{rules.maxFeatures}}.
-          </v-alert>
+          </div>
+          <div v-if="!errors.textData">
+            <v-icon color="green" >mdi-check-circle</v-icon> All rows have numerical data.
+          </div>
         </div>
-      </div>
+      </v-card>
 
-      <div>
-        <div v-if="errors.maxTrainingRows">
-          <v-alert dense outlined color="error">
-            Data set has {{fileData.rows}} rows. Max is {{rules.maxTrainingRows}}.
-          </v-alert>
-        </div>
-      </div>
-
-      <div>
-        <div v-if="errors.maxTestingRows">
-          <v-alert dense outlined color="error">
-            Data set has {{fileData.rows}} rows. Max is {{rules.maxTestingRows}}.
-          </v-alert>
-        </div>
-      </div>
 
 
 
