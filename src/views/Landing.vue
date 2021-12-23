42ac879f79fc163418e8223648c6f81a03fed49b
bdfennell <brandon.fennell@gmail.com>
2021-12-23 09:18:23 -0800

    makes tools hide-able

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,19 +1,23 @@
 <template>
   <v-container>
-        <v-card
-          class="my-5 pa-5 text-center"
-          v-for="(data, name) in $store.state.tools" :key="name"
-          hover
-          outlined
-          rounded
-          @click="$router.push({name: name})"
-          >
-          <v-icon x-large>{{data.icon}}</v-icon>
-          <span class="title">{{data.title}}</span>
-          <div class="caption">
-            {{data.description}}
-          </div>
-        </v-card>
+    <div v-for="(data, name) in $store.state.tools" :key="name">
+      <v-card
+        class="my-5 pa-5 text-center"
+        hover
+        outlined
+        rounded
+        v-if="data.isVisible"
+        @click="$router.push({name: name})"
+        >
+        <v-icon x-large>{{data.icon}}</v-icon>
+        <span class="title">{{data.title}}</span>
+        <div class="caption">
+          {{data.description}}
+        </div>
+      </v-card>
+
+    </div>
+
   </v-container>
 </template>
 <script>
