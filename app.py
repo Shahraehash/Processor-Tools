dfc6c5623b6b0f582d15a16da2cbc810aa4af3e4
bdfennell <brandon.fennell@gmail.com>
2021-12-25 07:31:56 -0800

    update

--- app.py
+++ app.py
@@ -630,65 +630,32 @@ def encode_store():
 
     #Script will try to read file and return data. If it fails it will delete
     #the file.
-    try:
-        df = pd.read_csv(file_path)
+    # try:
+    df = pd.read_csv(file_path)
+    print(df.shape)
 
-        valid_data_types = ['int64','float64']
-        invalid_columns = []
 
-        for item in df.dtypes.keys():
-            if df.dtypes[item] not in valid_data_types:
-                invalid_columns.append(item)
 
-        #params
-        skew = df.skew()
-        skew.name = 'skew'
+    entry = {
+    'user_id': 'ui000001',
+    # 'storage_id': storage_id,
+    # 'file_name':  file_name,
+    }
 
-        describe = df.describe().append(skew).transpose()
-        describe.reset_index(inplace=True)
-        describe = describe.rename(columns={'index':'feature'})
-        describe = describe.round({
-            'mean': 1,
-            'std': 1,
-            'min': 1,
-            '25%': 1,
-            '50%': 1,
-            '75%': 1,
-            'max': 1,
-            'skew': 1
-        })
+    db.insert(entry)
 
-        entry = {
-        'user_id': 'ui000001',
-        'storage_id': storage_id,
-        'file_name':  file_name,
-        'content_type': file_obj.content_type,
-        'upload_time': datetime.timestamp(datetime.now()),
-        'rows': int(df.shape[0]),
-        'columns': int(df.shape[1]),
-        'column_names': list(df.columns.values),
-        'column_names_reversed': list(np.flip(df.columns.values)),
-        'nan_count': int(find_nan_counts(df)),
-        'dtypes_count': json.loads(df.dtypes.value_counts().to_json()),
-        'nan_by_column': json.loads(df.isna().sum().to_json()),
-        'invalid_columns': list(invalid_columns),
-        'describe': describe.to_json(orient="records")
-        }
+    response = make_response(
+        jsonify(entry),
+        200,
+    )
+    response.headers["Content-Type"] = "application/json"
+    time.sleep(1)
+    return response
 
-        db.insert(entry)
-
-        response = make_response(
-            jsonify(entry),
-            200,
-        )
-        response.headers["Content-Type"] = "application/json"
-        time.sleep(1)
-        return response
-
-    except Exception as e:
-
-        os.remove(file_path)
-        return abort(500)
+    # except Exception as e:
+    #
+    #     os.remove(file_path)
+    #     return False
 
 
 
