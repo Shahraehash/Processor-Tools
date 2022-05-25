5dd9aae48f58af1e5e02355c189d960989a97217
bdfennell <brandon.fennell@gmail.com>
2022-05-24 19:54:49 -0700

    fix radio button issue

--- src/components/v2/StepFileRow.vue
+++ src/components/v2/StepFileRow.vue
@@ -12,7 +12,7 @@
         <div class="ml-3">
           {{count}} row<span v-if="count > 1 || count == 0">s</span> with missing values 
           <div>{{Object.keys(filePipeline.columnAdjust.nan_columns[file]).length}} columns with missing values</div>
-          <div class="pl-5" v-for="(val, col) in filePipeline.columnAdjust.nan_columns[file]" :key="val">
+          <div class="pl-5" v-for="(val, col) in filePipeline.columnAdjust.nan_columns[file]" :key="col">
           {{col}}: {{val}}
           </div>
         </div>
@@ -25,10 +25,10 @@
         class="mt-0"
         v-model="filePipeline.rowOption"
       >
-        <v-radio label="Remove rows with missing data"></v-radio>
-        <v-radio label="Impute rows with missing data"></v-radio>
-        <v-switch class="mt-2 right" label="Include indexs in export" v-model="includeIndexes"></v-switch>
+        <v-radio label="Remove rows with missing data" value="0"></v-radio>
+        <v-radio label="Impute rows with missing data" value="1"></v-radio>
       </v-radio-group>
+      <v-switch class="mt-2 right" label="Include indexs in export" v-model="includeIndexes"></v-switch>
     </div>
     <div v-else>
       <v-icon color="green" >mdi-check-circle</v-icon> No missing rows.
