5dad2a6406ca677aaadf466c239bb78d6e0f2774
fennell <bfennell@milo-ml.com>
2022-12-01 20:45:49 -0800

    fix file names

--- src/components/v3miniValidateFileSizes.vue
+++ src/components/v3miniValidateFileSizes.vue
@@ -1,35 +1,14 @@
 <template>
     <div>
-        <div v-if="analysis && false">
-            <div v-for="(value, key) in analysis.groupResults" :key="key">
-                <div class="red--text">IN PROGRESS</div>
-                {{key}}
-                <div v-for="(subvalue, subkey) in value" :key="subkey">
-                    <div>{{subkey}}: {{subvalue}}</div>
+        <div v-if="analysis">
+            
+            <v-row>
+                <v-col cols="6">
+                    <v-card outlined>
 
-                </div>
-                <!-- <div>
-                    Sufficient Training Data Size: {{analysis.groupResults.combined.testing_size_valid}}
-                </div>
-                <div>
-                    Sufficient Testing Data Size: {{analysis.groupResults.combined.testing_size_valid}}
-                </div>
-                <div>
-                    Sufficient Total Data Size: {{analysis.groupResults.combined.total_size_valid}}
-                </div>   
-                <div>
-                    rows_max_valid: {{analysis.groupResults.combined.rows_max_valid}}
-                </div>   
-                <div>
-                    cols_max_valid: {{analysis.groupResults.combined.cols_max_valid}}
-                </div>                                                
-                 -->
-                
-            </div>
-
-        </div>
-        <div v-else>
-            ...adding additional checks...
+                    </v-card>
+                </v-col>
+            </v-row>
 
         </div>
     </div>
